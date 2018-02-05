5afa6499572a7abfdc91e26e66e9f07f37527131
HajerBA <hajerbnch@gmail.com>
2018-02-05 14:14:16 +0100

    add form cnx

--- default/src/routes.php
+++ default/src/routes.php
@@ -14,3 +14,10 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
         'args' => $args
     ]);
 })->setName('index');
+$app->post('/login', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/login' route");
+    $body = $request->getParsedBody();
+    $body['is_logged'] = ($body['mail'] === "hajer@gmail.com" && $body['pwd'] === "pass");
+    return $this->view->render($response, 'login.twig', $body);
+});
